79b380584e816d6ca9f17ebc3d522a4b7ffab691
Tin Nguyen <king28112005@gmail.com>
2024-08-16 17:39:26 +0700

    Add files via upload

---
+++ Welcome2.java
@@ -0,0 +1,7 @@
+public class Welcome2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Tin Nguyen!");
+        System.out.println("Welcome to the DHKTPM19ATT!");
+    }
+}
+
